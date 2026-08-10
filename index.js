9d0b487a8f09e1d44cab1220fe19acc43827e4a5
tavakolishaghayegh <tavakolishaghayegh36@gmail.com>
2026-08-10 22:57:14 +0330

    show popup after delay and close it on click

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
- 
+// popup
+
+const popup = document.querySelector("#popup");
+setTimeout(() => {
+  popup.classList.add("popup");
+}, 3000);
+
+function closePopup() {
+  popup.classList.remove("popup");
+}
+popup.addEventListener("click", closePopup);
