1b8f8912fa72660ec1a7b32cf57384e1901f0831
tavakolishaghayegh <tavakolishaghayegh36@gmail.com>
2026-08-17 01:56:15 +0330

    Menu Button

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // popup
 
-const popup = document.querySelector("#popup");
-setTimeout(() => {
-  popup.classList.add("popup");
-}, 3000);
+// const popup = document.querySelector("#popup");
+// setTimeout(() => {
+//   popup.classList.add("popup");
+// }, 3000);
 
-function closePopup() {
-  popup.classList.remove("popup");
+// function closePopup() {
+//   popup.classList.remove("popup");
+// }
+// popup.addEventListener("click", closePopup);
+
+//Burger Menu
+const burger = document.querySelector("#burger");
+function menu() {
+  const navItems = document.querySelectorAll(".nav-item");
+  for (const navItem of navItems) {
+    if (navItem.classList.contains("nav-item-show")) {
+      navItem.classList.remove("nav-item-show");
+    } else {
+      navItem.classList.add("nav-item-show");
+    }
+  }
 }
-popup.addEventListener("click", closePopup);
+burger.addEventListener("click", menu);
